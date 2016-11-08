7bb891a97320cc0c98e4d53372b33361c55f0ee6
CPNV-SC\Antoine.DESSAUGES <antoine.dessauges@cpnv.ch>
2016-11-08 10:35:22 +0100

    Add - Play movie and percent grid
    Button to start movie with default software and start display single movie in a percent grid

--- Find My Movie/Find My Movie/MainWindow.xaml.cs
+++ Find My Movie/Find My Movie/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 using System.Windows.Media.Animation;
 using System.IO;
+using System.Diagnostics;
 
 namespace Find_My_Movie {
     /// <summary>
@@ -49,6 +50,7 @@ namespace Find_My_Movie {
 
             IEnumerable<Image> covers = gridMovies.Children.OfType<Image>();
             double maxWidth = interfaceClass.getWidthMovie(containerMovies.ActualWidth);
+            single.Width = containerMovies.ActualWidth;
             foreach (Image child in covers) {
                 child.MaxWidth = maxWidth;
             }//foreach
@@ -117,6 +119,7 @@ namespace Find_My_Movie {
             //display cover
             int i = 0;
             double maxWidth = interfaceClass.getWidthMovie(containerMovies.ActualWidth);
+            single.Width = containerMovies.ActualWidth;
             foreach (var cover in moviesCover) {
                 var webImage = new BitmapImage(new Uri(cover));
                 var imageControl = new Image();
@@ -129,6 +132,15 @@ namespace Find_My_Movie {
             }
 
         }//MetroWindow_Loaded
+
+        private void play_MouseUp (object sender, MouseButtonEventArgs e) {
+           
+            string pathMovie = "C:\\Users\\Antoine.DESSAUGES\\Documents\\Projets\\Wildlife.wmv";
+            if (File.Exists(pathMovie))
+                Process.Start(pathMovie);
+            else
+                MessageBox.Show("Erreur : Chemin vers le fichier incorecte !");
+        }
 
         void displaySingleMovie (object sender, MouseEventArgs e) {
 

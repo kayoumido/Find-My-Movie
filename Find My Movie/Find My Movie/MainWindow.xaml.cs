676c10ff4aedf201f85d5cb3b85b7768b903e9e2
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-11-18 11:41:34 +0100

    Add - SQLite package to project and created a reference to it
    It's possible that you might need to edit reference so it can work on your machine

--- Find My Movie/Find My Movie/MainWindow.xaml.cs
+++ Find My Movie/Find My Movie/MainWindow.xaml.cs
@@ -105,13 +105,13 @@ namespace Find_My_Movie {
                 Movie infos    = api.GetMovieInfo();
 
                 Credits credit = api.GetMovieCredits();
-
             }
 
             //Hard Display
             string[] moviesCover = new string[] {
                                             "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
                                             "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
+            };
         }
     }
 }

4013fe4e86ae419751fb08078473c5368fdae5e8
CPNV-SC\Struan.FORSYTH <struan.forsyth@cpnv.ch>
2016-11-26 14:12:08 +0100

    Add - Class cast
    Added the class for the cast

---
+++ Find My Movie/Find My Movie/classes/cast.class.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Find_My_Movie;
+
+namespace classes {
+    class cast {
+
+        private int id;
+        private int castId;
+        private string creditId;
+        private string name;
+        private string image;
+        private string character;
+        private int order;
+
+        public int Id {
+            get {
+                return id;
+            }
+
+            set {
+                id = value;
+            }
+        }
+
+        public int CastId {
+            get {
+                return castId;
+            }
+
+            set {
+                castId = value;
+            }
+        }
+
+        public string CreditId {
+            get {
+                return creditId;
+            }
+
+            set {
+                creditId = value;
+            }
+        }
+
+        public string Name {
+            get {
+                return name;
+            }
+
+            set {
+                name = value;
+            }
+        }
+
+        public string Image {
+            get {
+                return image;
+            }
+
+            set {
+                image = value;
+            }
+        }
+
+        public string Character {
+            get {
+                return character;
+            }
+
+            set {
+                character = value;
+            }
+        }
+
+        public int Order {
+            get {
+                return order;
+            }
+
+            set {
+                order = value;
+            }
+        }
+    }
+
+}

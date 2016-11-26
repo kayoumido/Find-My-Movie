8e7f516ffd35cdafb7b3123e0e6284b01a160982
CPNV-SC\Struan.FORSYTH <struan.forsyth@cpnv.ch>
2016-11-26 14:15:06 +0100

    Add - Class crew
    Added the class for the crew

---
+++ Find My Movie/Find My Movie/classes/crew.class.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Find_My_Movie;
+
+namespace classes {
+    class crew {
+
+        private int id;
+        private string creditId;
+        private string name;
+        private string image;
+
+        public int Id {
+            get {
+                return id;
+            }
+
+            set {
+                id = value;
+            }
+        }
+
+        public string CreditId {
+            get {
+                return creditId;
+            }
+
+            set {
+                creditId = value;
+            }
+        }
+
+        public string Name {
+            get {
+                return name;
+            }
+
+            set {
+                name = value;
+            }
+        }
+
+        public string Image {
+            get {
+                return image;
+            }
+
+            set {
+                image = value;
+            }
+        }
+    }
+
+}

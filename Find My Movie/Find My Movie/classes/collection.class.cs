350cdf52f68e469817c06a8d8aa7719fd766dad3
CPNV-SC\Struan.FORSYTH <struan.forsyth@cpnv.ch>
2016-11-26 13:29:36 +0100

    Add - Class collection
    Added the class for the collection

---
+++ Find My Movie/Find My Movie/classes/collection.class.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Find_My_Movie;
+
+namespace classes {
+    class collection {
+
+        private int id;
+        private string name;
+        private string poster;
+
+        public collection() {
+
+        }
+
+        public int Id {
+            get {
+                return id;
+            }
+
+            set {
+                id = value;
+            }
+        }
+
+        public string Name {
+            get {
+                return name;
+            }
+
+            set {
+                name = value;
+            }
+        }
+
+        public string Poster {
+            get {
+                return poster;
+            }
+
+            set {
+                poster = value;
+            }
+        }
+    }
+}

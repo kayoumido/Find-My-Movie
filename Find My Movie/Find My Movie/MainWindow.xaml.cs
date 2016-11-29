2aaa3046f885028e2ea4d0e4d8f126f95f31c3a3
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-11-29 16:51:24 +0100

    Change - DBHandler - Abandoned for ActiveRecord

--- Find My Movie/Find My Movie/MainWindow.xaml.cs
+++ Find My Movie/Find My Movie/MainWindow.xaml.cs
@@ -113,10 +113,10 @@ namespace Find_My_Movie {
                     Movie infos    = api.GetMovieInfo();
                     Credits credit = api.GetMovieCredits();
 
-                    FMMDb.InsertInDB(infos, credit);
+                    // FMMDb.InsertInDB(infos, credit);
 
                     // check number of film founds
-                    if (foo == 1) {
+                    if (foo == 4) {
                         // break out of loop
                         // this is emporary code
                         break;
@@ -131,22 +131,22 @@ namespace Find_My_Movie {
 
             //Hard Display
             string[] moviesCover = new string[] {
-                                            "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
-                                            "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
-                                            "http://violentworldofparker.com/wordpress/wp-content/uploads/2012/10/Flashfire2013.jpg",
-                                            "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
-                                            "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
-                                            "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
-                                            "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
-                                            "http://violentworldofparker.com/wordpress/wp-content/uploads/2012/10/Flashfire2013.jpg",
-                                            "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
-                                            "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
-                                            "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
-                                            "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
-                                            "http://violentworldofparker.com/wordpress/wp-content/uploads/2012/10/Flashfire2013.jpg",
-                                            "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
-                                            "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
-                                            "http://violentworldofparker.com/wordpress/wp-content/uploads/2012/10/Flashfire2013.jpg"
+                "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
+                "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
+                "http://violentworldofparker.com/wordpress/wp-content/uploads/2012/10/Flashfire2013.jpg",
+                "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
+                "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
+                "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
+                "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
+                "http://violentworldofparker.com/wordpress/wp-content/uploads/2012/10/Flashfire2013.jpg",
+                "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
+                "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
+                "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
+                "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
+                "http://violentworldofparker.com/wordpress/wp-content/uploads/2012/10/Flashfire2013.jpg",
+                "https://s-media-cache-ak0.pinimg.com/236x/82/f0/15/82f01596145820a6f8ab76f191ae346d.jpg",
+                "https://jacobboombar.files.wordpress.com/2014/03/oblivion-dvd-cover-55.jpg",
+                "http://violentworldofparker.com/wordpress/wp-content/uploads/2012/10/Flashfire2013.jpg"
             };
 
             //display cover

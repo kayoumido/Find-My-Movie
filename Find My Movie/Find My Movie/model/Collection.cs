e99b39cdc619e6c448e06d0e61d7b43d8752fb57
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-12-06 16:32:13 +0100

    Change - collection file and class name
    Was Collextion now Collection

--- Find My Movie/Find My Movie/model/Collection.cs
+++ Find My Movie/Find My Movie/model/Collection.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Find_My_Movie.model {
-    public class Collextion {
+    public class Collection {
         public int id { get; set; }
         public string name { get; set;}
         public string poster { get; set; }

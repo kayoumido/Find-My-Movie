9258011c9083c18dc5dd0ed984a9465baf34fd74
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-12-06 16:50:36 +0100

    Add - Created Cast and Collection repository

---
+++ Find My Movie/Find My Movie/model/dal/ICastRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_My_Movie.model.dal {
+    interface ICastRepository {
+        List<Cast> GetCasts(string sort);
+
+        Cast GetCast(int id);
+
+        bool Insert(Cast cast);
+
+        bool Delete(int id);
+    }
+}
---
+++ Find My Movie/Find My Movie/model/dal/ICollectionRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_My_Movie.model.dal {
+    interface ICollectionRepository {
+        List<Collection> GetCollections(string sort);
+
+        Collection GetCollection(int id);
+
+        bool Insert(Collection collection);
+
+        bool Delete(int id);
+    }
+}

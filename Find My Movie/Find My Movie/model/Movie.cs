0e0dbecef21af82634b50c864c5fac8162f8524f
CPNV-SC\Struan.FORSYTH <struan.forsyth@cpnv.ch>
2016-12-06 16:10:07 +0100

    Add - class movie
    Added the class for the movie object

---
+++ Find My Movie/Find My Movie/model/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_My_Movie.model {
+    class Movie {
+
+        public int id { get; set; }
+        public string imdbid { get; set; }
+        public string title { get; set; }
+        public string ogtitle { get; set; }
+        public string alttitle { get; set; }
+        public bool adult { get; set; }
+        public int budget { get; set; }
+        public string homepage { get; set; }
+        public int runtime { get; set; }
+        public string tagline { get; set; }
+        public float voteaverage { get; set; }
+        public string oglanguage { get; set; }
+        public string overview { get; set; }
+        public float popularity { get; set; }
+        public string poster { get; set; }
+        public string releasedate { get; set; }
+        public int fk_collection { get; set; }
+
+
+    }
+
+}

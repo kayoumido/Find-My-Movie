138872f2f3d1bdaa22886376e19c0105a2bea9ba
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-12-09 12:09:59 +0100

    Add - implemented insert Genre

--- Find My Movie/Find My Movie/model/repository/GenreRepository.cs
+++ Find My Movie/Find My Movie/model/repository/GenreRepository.cs
@@ -1,12 +1,19 @@
 ﻿using Find_My_Movie.model.dal;
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using Dapper;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Find_My_Movie.model.repository {
     class GenreRepository : IGenreRepository {
+
+        static private dbhandler dbh = new dbhandler();
+
+        private SQLiteConnection DBConnection = dbh.Connect();
+
         public List<fmmGenre> GetGenres(string sort) {
             throw new NotImplementedException();
         }
@@ -16,7 +23,22 @@ namespace Find_My_Movie.model.repository {
         }
 
         public bool Insert(fmmGenre genre) {
-            throw new NotImplementedException();
+            int rowsAffected = this.DBConnection.Execute(@"
+                INSERT OR IGNORE INTO
+                    genre 
+                VALUES (
+                    @id, 
+                    @name
+                );",
+                genre
+            );
+
+            dbh.Disconnect(DBConnection);
+
+            if (rowsAffected > 0) {
+                return true;
+            }
+            return false;
         }
 
         public bool Delete(int id) {

09b7e80dcec09c7c197866fc5e8fe15b09810fc0
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-12-09 12:09:48 +0100

    Add - implemented insert crew

--- Find My Movie/Find My Movie/model/repository/CrewRepository.cs
+++ Find My Movie/Find My Movie/model/repository/CrewRepository.cs
@@ -1,12 +1,20 @@
-﻿using Find_My_Movie.model.dal;
+﻿using Dapper;
+using Find_My_Movie.model.dal;
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Find_My_Movie.model.repository {
     class CrewRepository : ICrewRepository {
+
+        static private dbhandler dbh = new dbhandler();
+
+        private SQLiteConnection DBConnection = dbh.Connect();
+        
+
         public List<fmmCrew> GetCrews(string sort) {
             throw new NotImplementedException();
         }
@@ -15,8 +23,33 @@ namespace Find_My_Movie.model.repository {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Insert a Crew in database
+        /// </summary>
+        /// <param name="crew">fmmCrew object to insert in db</param>
+        /// <returns>True if insert was successful otherwise false</returns>
+        /// 
+        /// <author>Doran Kayoumi</author>
         public bool Insert(fmmCrew crew) {
-            throw new NotImplementedException();
+
+            int rowsAffected = this.DBConnection.Execute(@"
+                INSERT OR IGNORE INTO
+                    crew 
+                VALUES (
+                    @id, 
+                    @creditid, 
+                    @name,
+                    @image
+                );",
+                crew
+            );
+
+            dbh.Disconnect(DBConnection);
+
+            if (rowsAffected > 0) {
+                return true;
+            }
+            return false;
         }
 
         public bool Delete(int id) {

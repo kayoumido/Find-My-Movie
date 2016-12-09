4ff77ed53d988cb08d5f8ecc9d7532244d5d510f
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-12-09 12:09:00 +0100

    Add - implemented Insert cast

--- Find My Movie/Find My Movie/model/repository/CastRepository.cs
+++ Find My Movie/Find My Movie/model/repository/CastRepository.cs
@@ -1,12 +1,19 @@
-﻿using Find_My_Movie.model.dal;
+﻿using Dapper;
+using Find_My_Movie.model.dal;
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Find_My_Movie.model.repository {
     public class CastRepository : ICastRepository {
+
+        static private dbhandler dbh = new dbhandler();
+
+        private SQLiteConnection DBConnection = dbh.Connect();
+
         public List<fmmCast> GetCasts(string sort) {
             throw new NotImplementedException();
         }
@@ -16,7 +23,26 @@ namespace Find_My_Movie.model.repository {
         }
 
         public bool Insert(fmmCast cast) {
-            throw new NotImplementedException();
+
+            int rowsAffected = this.DBConnection.Execute(@"
+                INSERT OR IGNORE INTO
+                    cast 
+                VALUES (
+                    @id,
+                    @castid,
+                    @creditid, 
+                    @name,
+                    @image
+                );",
+                cast
+            );
+
+            dbh.Disconnect(DBConnection);
+
+            if (rowsAffected > 0) {
+                return true;
+            }
+            return false; 
         }
 
         public bool Delete(int id) {

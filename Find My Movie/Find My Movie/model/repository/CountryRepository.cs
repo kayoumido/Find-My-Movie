6b080c13a810125a743f3209b2a790edfb7a4885
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-12-09 12:09:15 +0100

    Add - implemented insert country

--- Find My Movie/Find My Movie/model/repository/CountryRepository.cs
+++ Find My Movie/Find My Movie/model/repository/CountryRepository.cs
@@ -1,12 +1,18 @@
 ﻿using Find_My_Movie.model.dal;
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using Dapper;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Find_My_Movie.model.repository {
     class CountryRepository : ICountryRepository {
+        static private dbhandler dbh = new dbhandler();
+
+        private SQLiteConnection DBConnection = dbh.Connect();
+
         public List<fmmCountry> GetCountries(string sort) {
             throw new NotImplementedException();
         }
@@ -16,7 +22,25 @@ namespace Find_My_Movie.model.repository {
         }
 
         public bool Insert(fmmCountry country) {
-            throw new NotImplementedException();
+
+            int rowsAffected = this.DBConnection.Execute(@"
+                INSERT OR IGNORE INTO
+                    country(
+                        name                    
+                    )
+                VALUES (
+                    @name
+                );",
+                country
+            );
+
+            dbh.Disconnect(DBConnection);
+
+            if (rowsAffected > 0) {
+                return true;
+            }
+            return false;
+            // throw new NotImplementedException();
         }
 
         public bool Delete(int id) {

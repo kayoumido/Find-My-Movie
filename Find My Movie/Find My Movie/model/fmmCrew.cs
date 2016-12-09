0c0708c4c211f45308a02a1c63f76f0ad611f789
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-12-09 12:08:40 +0100

    Add - creditid to Crew class

--- Find My Movie/Find My Movie/model/fmmCrew.cs
+++ Find My Movie/Find My Movie/model/fmmCrew.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Find_My_Movie.model {
     public class fmmCrew {
         public int id { get; set; }
+        public string creditid { get; set; }
         public string name { get; set; }
         public string image { get; set; }
     }

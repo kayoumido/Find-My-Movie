b001b87a060e208dfd4b5a9048d27178e22a8ae7
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-12-09 12:04:08 +0100

    Add - created Repsoitories for Language

---
+++ Find My Movie/Find My Movie/model/dal/ILanguageRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_My_Movie.model.dal {
+    interface ILanguageRepository {
+        List<fmmLanguage> GetLanguages(string sort);
+
+        fmmLanguage GetLanguage(int id);
+
+        bool Insert(fmmLanguage language);
+
+        bool Delete(int id);
+    }
+}
---
+++ Find My Movie/Find My Movie/model/repository/LanguageRepository.cs
@@ -0,0 +1,48 @@
+﻿using Find_My_Movie.model.dal;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using Dapper;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_My_Movie.model.repository {
+    public class LanguageRepository : ILanguageRepository {
+
+        static private dbhandler dbh = new dbhandler();
+
+        private SQLiteConnection DBConnection = dbh.Connect();
+
+        public List<fmmLanguage> GetLanguages(string sort) {
+            throw new NotImplementedException();
+        }
+
+        public fmmLanguage GetLanguage(int id) {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(fmmLanguage language) {
+            int rowsAffected = this.DBConnection.Execute(@"
+                INSERT OR IGNORE INTO
+                    language 
+                VALUES (
+                    @name
+                );",
+                language
+            );
+
+            dbh.Disconnect(DBConnection);
+
+            if (rowsAffected > 0) {
+                return true;
+            }
+            return false;
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id) {
+            throw new NotImplementedException();
+        }
+    }
+}

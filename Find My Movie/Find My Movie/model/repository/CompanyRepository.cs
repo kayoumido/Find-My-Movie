955b9abacffd657882d6e8144e09e8f3b822867b
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-12-09 12:09:36 +0100

    Add - implemented insert company

--- Find My Movie/Find My Movie/model/repository/CompanyRepository.cs
+++ Find My Movie/Find My Movie/model/repository/CompanyRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Find_My_Movie.model.repository {
     class CompanyRepository {
+        static private dbhandler dbh = new dbhandler();
+
+        private SQLiteConnection DBConnection = dbh.Connect();
+
         public List<fmmCompany> GetCompanies(string sort) {
             throw new NotImplementedException();
         }
@@ -15,7 +21,24 @@ namespace Find_My_Movie.model.repository {
         }
 
         public bool Insert(fmmCompany company) {
-            throw new NotImplementedException();
+
+            int rowsAffected = this.DBConnection.Execute(@"
+                INSERT OR IGNORE INTO
+                    company 
+                VALUES (
+                    @id,
+                    @name
+                );",
+                company
+            );
+
+            dbh.Disconnect(DBConnection);
+
+            if (rowsAffected > 0) {
+                return true;
+            }
+            return false;
+
         }
 
         public bool Delete(int id) {

492f15e32e8310f36e6fe47287651f9141e180e9
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2016-12-12 18:14:59 +0100

    Change - added new fields to Crew object
    This was done to help with inserting in DB

--- Find My Movie/Find My Movie/model/fmmCrew.cs
+++ Find My Movie/Find My Movie/model/fmmCrew.cs
@@ -10,5 +10,7 @@ namespace Find_My_Movie.model {
         public string creditid { get; set; }
         public string name { get; set; }
         public string image { get; set; }
+        public string department { get; set; }
+        public string job { get; set; }
     }
 }
